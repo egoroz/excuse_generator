0f21b57d01f5931cbaff8616c971abd92ef43d78
egororo <egorsemirenko04@gmail.com>
2025-04-03 13:10:54 +0300

    :)

--- src/database.cpp
+++ src/database.cpp
@@ -1,15 +1,11 @@
 #include "../include/database.h"
 
-Storage InitStorage(const std::string& path_to_db = "excudes.db"){
+Storage InitStorage(const std::string& path_to_db){
     return sq::make_storage(path_to_db, 
                 sq::make_table("excuse_components",
-                    sq::make_column("id", &ExcuseComponent::id),
-                    sq::make_column("component_type", &ExcuseComponent::component_type);
-                    sq::make_column("text", &ExcuseComponent::text);
+                    sq::make_column("id", &ExcuseComponent::id, sq::primary_key().autoincrement()),
+                    sq::make_column("component_type", &ExcuseComponent::type),
+                    sq::make_column("text", &ExcuseComponent::text)
         )
     );
-}
-
-Storage StoreStorage(const std::string& path_to_db = "excudes.db"){
-
 }
