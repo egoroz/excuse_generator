82a9b0cc47105e7941766455ea3838ea9783f1d7
egororo <egorsemirenko04@gmail.com>
2025-04-03 19:08:23 +0300

    fix safety

--- src/main.cpp
+++ src/main.cpp
@@ -13,14 +13,23 @@ int main(){
     storage.sync_schema();
 
     CROW_ROUTE(app, "/generate")
-    ([&storage]() {
-        std::string excuse = GenerateExcuse(storage, ExcuseType::CANT_HELP, Gender::FEMALE);
-        nlohmann::json response_json;
-        response_json["excuse"] = excuse;
-        crow::response response(response_json.dump());
-        response.set_header("Content-Type", "application/json; charset=utf-8"); 
-        return response;
-        });
+    ([&storage](const crow::request& req) {
+
+        std::string origin = req.get_header_value("Origin");
+        if (origin == "http://localhost:8000") {  // Access only through localhost:8000
+            std::string excuse = GenerateExcuse(storage, ExcuseType::CANT_HELP, Gender::FEMALE);
+            nlohmann::json response_json;
+            response_json["excuse"] = excuse;
+            crow::response response(response_json.dump());
+
+            response.add_header("Content-Type", "application/json; charset=utf-8");
+            response.add_header("Access-Control-Allow-Origin", origin);
+
+            return response;
+        } else {
+            return crow::response(403); // Forbidden
+        }
+    });
 
     app.port(1234)
        .multithreaded()
